4e2aea83731a7548020944ca1a8cc53846662e51
Daymond Blair <mondday@Daymonds-MacBook-Pro.local>
2024-01-17 22:37:18 -0800

    created initial layout

--- src/App.js
+++ src/App.js
@@ -1,41 +1,22 @@
 const App = () => {
+	const categories = [
+		{ id: 1, title: 'Hats' },
+		{ id: 2, title: 'Jackets' },
+		{ id: 3, title: 'Sneakers' },
+		{ id: 4, title: 'Womens' },
+		{ id: 5, title: 'Mens' },
+	]
 	return (
 		<div className="categories-container">
-			<div className="category-container">
-				{/* {<img></img>} */}
-				<div className="category-body-container">
-					<h1>Hats</h1>
-					Shop Now
+			{categories.map(({ title }) => (
+				<div className="category-container">
+					{/* {<img></img>} */}
+					<div className="category-body-container">
+						<h2>{title}</h2>
+						<p>Shop Now</p>
+					</div>
 				</div>
-			</div>
-			<div className="category-container">
-				{/* {<img></img>} */}
-				<div className="category-body-container">
-					<h1>Jackets</h1>
-					Shop Now
-				</div>
-			</div>
-			<div className="category-container">
-				{/* {<img></img>} */}
-				<div className="category-body-container">
-					<h1>Sneakers</h1>
-					Shop Now
-				</div>
-			</div>
-			<div className="category-container">
-				{/* {<img></img>} */}
-				<div className="category-body-container">
-					<h1>Womens</h1>
-					Shop Now
-				</div>
-			</div>
-			<div className="category-container">
-				{/* {<img></img>} */}
-				<div className="category-body-container">
-					<h1>Mens</h1>
-					Shop Now
-				</div>
-			</div>
+			))}
 		</div>
 	)
 }
