3c3cef4026cc31095f3396856c1cb74e278bfe42
Daymond Blair <mondday@Daymonds-MacBook-Pro.local>
2024-02-13 01:20:24 -0800

    created sign in component and added it to navigation route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './components/categories/categories.styles.scss'
 import Home from './components/routes/home/home.component'
 import Navigation from './components/routes/navigation/navigation.component'
 import { Routes, Route } from 'react-router-dom'
+import SignIn from './components/sign-in/sign-in.component'
 
 const Shop = () => {
 	return <h1>Here's the shop component</h1>
@@ -13,6 +14,7 @@ const App = () => {
 			<Route path="/" element={<Navigation />}>
 				<Route index element={<Home />} />
 				<Route path="shop" element={<Shop />} />
+				<Route path="sign-in" element={<SignIn />} />
 			</Route>
 		</Routes>
 	)
