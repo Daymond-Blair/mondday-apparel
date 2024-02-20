bcb04a7ed81738845c7f98583ed1633382927e59
Daymond Blair <mondday@Daymonds-MacBook-Pro.local>
2024-02-19 23:58:53 -0800

    setting up firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import {
+	getAuth,
+	signInWithPopup,
+	signInWithRedirect,
+	GoogleAuthProvider,
+} from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyBd8wMdy67Sfa9j0qKz7Lk928CkHI8P03k',
+	authDomain: 'mondday-apparel.firebaseapp.com',
+	projectId: 'mondday-apparel',
+	storageBucket: 'mondday-apparel.appspot.com',
+	messagingSenderId: '161298098273',
+	appId: '1:161298098273:web:31e3217affa293f3776220',
+}
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
+
+const provider = new GoogleAuthProvider()
+
+provider.setCustomParameters({ prompt: 'select_account' })
+
+export const auth = getAuth()
+
+export const signInWithGooglePopup = signInWithPopup(auth, provider)
